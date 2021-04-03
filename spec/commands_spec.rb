6532632c1ef066d49cf4ea502e459c7ec850f6db
Toby Clemson <tobyclemson@gmail.com>
2021-04-03 21:28:48 +0100

    Add rubocop.

--- spec/commands_spec.rb
+++ spec/commands_spec.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe 'commands' do
   before(:all) do
     set :backend, :docker
-    set :docker_image, "alpine-aws-s3-config:latest"
+    set :docker_image, 'alpine-aws-s3-config:latest'
     set :docker_container_create_options, {
-        "Entrypoint" => "/bin/sh"
+      'Entrypoint' => '/bin/sh'
     }
   end
 
-  ['bash', 'curl', 'dumb-init'].each do |apk|
+  %w[bash curl dumb-init].each do |apk|
     it "includes #{apk}" do
       expect(package(apk)).to be_installed
     end
